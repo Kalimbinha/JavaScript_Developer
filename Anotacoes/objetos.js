472e88ceaf9b3a60dcdb9e3190d5fd68c0db3bb0
Fernando Barreto <152815498+Kalimbinha@users.noreply.github.com>
2025-03-19 10:55:59 -0300

    Objetos

---
+++ Anotacoes/objetos.js
@@ -0,0 +1,14 @@
+const fernando ={
+    nome: "Fernando Barreto Silva",
+    idade: 18,
+    namorada: "Isadora",
+    nivel: 3,
+
+    descrever: function () {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
+}
+
+const atributo = 'idade' // acessar de forma dinamica 
+
+console.log(fernando['nome'])
